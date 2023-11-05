e0f44b2cb4d8f6242f67949c46341431c575a098
SimpleSuyash <suyashmaharjan@gmail.com>
2023-11-05 23:18:37 +1100

    added and called the functions

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,31 @@
-var topics = ["HTML, CSS, Git, JavaScript" ];
+var topics = ["HTML", "CSS", "Git", "JavaScript" ];
+var randomTopic = topics[Math.floor(Math.random()*topics.length)];
 
-for (var x =0; x< topics.length;x++){
-   console.log(topics[x]);
+
+function listTopics(){
+    for (var x =0; x< topics.length; x++){
+        console.log(topics[x]);
+     }
 }
 
-/*
-var topic = "HTML";
-console.log(topics);
-if(topic==='HTML'){
-    console.log("Let's study HTML!");
-} else if(topic==='CSS'){
-    console.log("Let's study CSS!");
-} else if(topic==='Git'){
-    console.log("Let's study Git!");
-} else if(topic==='JavaScript'){
-    console.log("Let's study JavaScript!");
-} else{
-    console.log("Please try agian!");
+function selectTopic(){
+    var topic = "";
+    topic = randomTopic;
+    
+    if(topic==='HTML'){
+        console.log("Let's study HTML!");
+    } else if(topic==='CSS'){
+        console.log("Let's study CSS!");
+    } else if(topic==='Git'){
+        console.log("Let's study Git!");
+    } else if(topic==='JavaScript'){
+        console.log("Let's study JavaScript!");
+    } else{
+        console.log("Please try agian!");
+    }
 }
-*/
+
+console.log("Here are the topics we learned thought Prework: ");
+listTopics();
+console.log("Which topic should we study first?");
+selectTopic();
